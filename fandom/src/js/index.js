1a0408d902406d4630c4146827f06792f49453e0
Kirill Vinogradov <vikian050194@gmail.com>
2023-08-27 21:26:36 +0200

    fandom template small refactoring

--- fandom/src/js/index.js
+++ fandom/src/js/index.js
@@ -1,9 +1,8 @@
-// import "core-js/stable";
-// import "regenerator-runtime/runtime";
 import App from "./components/App";
 
 window.addEventListener("load", function() {
-    App("#root");
+    const $root = document.getElementById("root");
+    App($root);
 });
 
 console.info(`App is started at ${(new Date()).toLocaleString()}`);
